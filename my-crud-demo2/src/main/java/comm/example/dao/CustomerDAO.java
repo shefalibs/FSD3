b502bf7c14ad245943355e300615785d19094a7a
Shefali BS <shefalibs.09@gmail.com>
2019-10-21 17:30:11 +0530

    with search

---
+++ my-crud-demo2/src/main/java/comm/example/dao/CustomerDAO.java
@@ -0,0 +1,16 @@
+package comm.example.dao;
+
+import java.util.List;
+
+import comm.example.model.Customer;
+
+public interface CustomerDAO {
+	
+	public List<Customer> getAllCustomer();
+	public void createCustomer(Customer customer);
+	public void deleteCustomer(Customer customer);
+	public Customer getCustomerById(int id);
+	public void updateCustomer(int id,Customer c);
+	
+	public List<Customer> findCustomer(String str);
+}
