c451adba283c8952071195e6ba9fb73151612853
Shefali BS <shefalibs.09@gmail.com>
2019-10-22 12:54:52 +0530

    crud-session managemet

---
+++ my-crud-demo4/src/main/java/comm/example/controller/SortAscending.java
@@ -0,0 +1,53 @@
+package comm.example.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import comm.example.dao.CustomerDAO;
+import comm.example.dao.CustomerDAOImpl;
+import comm.example.model.Customer;
+
+
+@WebServlet("/ascending.do")
+public class SortAscending extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private CustomerDAO dao = null;
+	public void init() throws ServletException {
+		dao = new CustomerDAOImpl();
+	}
+	
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProccess(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProccess(request, response);
+	}
+	protected void doProccess(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		List<Customer> list=dao.sortAscending(request.getParameter("firstName"));
+		if(list.isEmpty())
+		{
+			RequestDispatcher view=request.getRequestDispatcher("list.do");
+			view.forward(request, response);
+		}
+		else
+		{
+			request.setAttribute("customers", list);
+			RequestDispatcher view=request.getRequestDispatcher("ascending.jsp");
+			view.forward(request, response);
+		}
+}
+}
---
+++ my-crud-demo4/src/main/webapp/index.jsp
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="ISO-8859-1">
+    <title>Login</title>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+  <h2>Login Customer</h2>
+ 
+  <form action="Login" method="post">
+    <div class="form-group">
+      <label for="uname">User name:</label>
+      <input type="text" class="form-control" id="uname" name="uname">
+    </div>
+    <div class="form-group">
+      <label for="pass">Password:</label>
+      <input type="password" class="form-control" id="pass" name="pass">
+    </div>
+    <input class="btn btn-info btn-block my-4" type="submit" value="submit"><br/>
+  </form>
+</div>
+
+</body>
+</html>
+
