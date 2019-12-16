f6fb1b7d7df3cc9954de864e1cbecee375a2cc92
Shefali BS <shefalibs.09@gmail.com>
2019-12-16 16:03:07 +0530

    Add files via upload

---
+++ Thymeleaf2/thymeleaf-demo/src/main/java/com/example/demo/service/EmployeeService.java
@@ -0,0 +1,19 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.entity.Employee;
+
+public interface EmployeeService {
+
+	public List<Employee> findAll();
+	
+	public Employee findById(int theId);
+	
+	public void save(Employee theEmployee);
+	
+	public void deleteById(int theId);
+
+	public List<Employee> searchBy(String theFirstName, String theLastName);
+	
+}
---
+++ Thymeleaf2/thymeleaf-demo/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO `employee`(id,first_name,last_name,email) VALUES 
+	(1,'Shefali','B S','shefali@gmail.com'),
+	(2,'Sreejitha','N','Sreejitha@gmail.com'),
+	(3,'Shreshta','Naik','shreshta@gmail.com'),
+	(4,'Navya','Prabhu','navya@gmail.com'),
+	(5,'Swarna','Acharya','swarna@gmail.com');
